a3e93acf3dfad076f92fdd357b7df9db44b14528
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-16 15:23:05 -0400

    pushing multiple controller changes

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,40 @@
 class PuppiesController < ApplicationController
-   
+    get '/puppies' do 
+        @puppies = Puppy.all 
+        erb :index 
+    end 
+    
+    get '/puppies/new' do 
+        erb :new
+    end 
+
+    post '/puppies/new' do 
+        redirect '/puppies/new'
+    end 
+
+    get '/puppies/:id' do 
+        @puppy = Puppy.find(params[:id])
+        erb :show 
+    end 
+
+    post '/puppies' do 
+        new_puppy = Puppy.create(params)
+        redirect '/puppies'
+    end 
+
+    get '/puppies/:id/edit' do 
+        @puppy = Puppy.find(params[:id])
+        erb :edit
+    end 
+
+    patch '/puppies/:id' do 
+        puppy = Puppy.find(params[:id])
+        puppy.update(name: params[:name], breed: params[:breed])
+        redirect "/puppies/#{puppy.id}"
+    end
+
+    delete '/puppies/:id' do 
+        Puppy.delete(params[:id])
+        redirect '/puppies'
+    end
 end
--- config.ru
+++ config.ru
@@ -3,7 +3,8 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
     raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
   end
   
-  
+require_relative './app/controllers/application_controller'
+require_relative './app/controllers/puppies_controller'
 
-
+use PuppiesController 
 run ApplicationController
