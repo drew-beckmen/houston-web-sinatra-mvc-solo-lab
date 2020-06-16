0ceb94895284ffa1a4ed605b6464f6df6ef89495
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-16 14:50:55 -0400

    CRUD operations on Puppy website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,43 @@ class ApplicationController < Sinatra::Base
         erb :welcome
       end
 
-   
+      get '/puppies' do 
+        @puppies = Puppy.all 
+        erb :index 
+      end 
+      
+      get '/puppies/new' do 
+        erb :new
+      end 
+
+      post '/puppies/new' do 
+        redirect '/puppies/new'
+      end 
+
+      get '/puppies/:id' do 
+        @puppy = Puppy.find(params[:id])
+        erb :show 
+      end 
+
+      post '/puppies' do 
+        new_puppy = Puppy.create(params)
+        redirect '/puppies'
+      end 
+
+      get '/puppies/:id/edit' do 
+        @puppy = Puppy.find(params[:id])
+        erb :edit
+      end 
+
+      patch '/puppies/:id' do 
+        puppy = Puppy.find(params[:id])
+        puppy.update(name: params[:name], breed: params[:breed])
+        redirect "/puppies/#{puppy.id}"
+      end
+
+      delete '/puppies/:id' do 
+        Puppy.delete(params[:id])
+        redirect '/puppies'
+      end 
+
 end
